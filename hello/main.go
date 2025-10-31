9c5287ff581d260d07621a9e609e6789085ee177
Salai Chit Oo Latt <salaichitoolatt@gmail.com>
2025-10-31 07:20:26 +0000

    Add HelloWorld Example

---
+++ hello/main.go
@@ -0,0 +1,10 @@
+package main
+
+import "fmt"
+
+func HelloWorld() {
+	fmt.Println("Hello world")
+}
+func main() {
+	HelloWorld()
+}
---
+++ hello/main_internal_test.go
@@ -0,0 +1,8 @@
+package main
+
+func ExampleHelloWorld() {
+	HelloWorld()
+
+	// Output:
+	// Hello world
+}
